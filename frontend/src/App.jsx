16bc7a25858ff0e66755696cb9fe449c08a81bf5
javierpb275 <javierpb275@gmail.com>
2024-03-05 17:54:02 +0100

    connected client to websocket with vite config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 import "./App.css";
 
-const socket = io("http://localhost:4000")
+const socket = io("/")
 
 function App() {
   return <div>hello world</div>;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:4000',
+        ws: true
+      }
+    }
+  }
 })
