b30504b3b412ebf77ec3e8da638fd0d8b73c3a24
javierpb275 <javierpb275@gmail.com>
2024-03-05 18:57:34 +0100

    listening to client events and emitting messages to other clients connected

--- server/index.js
+++ server/index.js
@@ -5,13 +5,16 @@ import { Server as SocketServer } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server, {
-    cors: {
-        origin: "http://localhost:5173"
-    }
+  cors: {
+    origin: "http://localhost:5173",
+  },
 });
 
 io.on("connection", (socket) => {
   console.log("client connected");
+  socket.on("message", (data) => {
+    socket.broadcast.emit("message", data);
+  });
 });
 
 server.listen(4000);
