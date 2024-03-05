7748ece9ab5b6579eca0964cf06552989259008c
javierpb275 <javierpb275@gmail.com>
2024-03-05 17:50:27 +0100

    connected client to websocket

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from "express";
-import http from 'http';
+import http from "http";
 import { Server as SocketServer } from "socket.io";
 
 const app = express();
-const server = http.createServer(app)
-const io = new SocketServer(server)
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+    cors: {
+        origin: "http://localhost:5173"
+    }
+});
 
+io.on("connection", (socket) => {
+  console.log("client connected");
+});
 
 server.listen(4000);
 console.log("Server on port", 4000);
